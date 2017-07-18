8b3ff9d91b66ba71aa860058565476f83de7bdca
Kirill G. Kotenko <zelder.rus@gmail.com>
2017-07-18 15:59:24 +0300

    test gitignore

--- src/scripts/datas/MockData.ts
+++ src/scripts/datas/MockData.ts
@@ -24,7 +24,7 @@ namespace Mock {
             new Transactions.TransactionLine().fromSome('1', _getDate(2017, 5, 10, 13, 20, 0), Transactions.TransactionTypes.Spend, "rub", 400),
             new Transactions.TransactionLine().fromSome('2', _getDate(2017, 5, 10, 20, 40, 0), Transactions.TransactionTypes.Spend, "rub", 2400),
             new Transactions.TransactionLine().fromSome('3', _getDate(2017, 5, 11, 10, 15, 0), Transactions.TransactionTypes.Profit, "rub", 55000),
-            new Transactions.TransactionLine().fromSome('4', _getDate(2017, 5, 11, 19, 50, 0), Transactions.TransactionTypes.Spend, "rub", 1750),
+            new Transactions.TransactionLine().fromSome('4', _getDate(2017, 5, 11, 19, 50, 0), Transactions.TransactionTypes.Spend, "rub", 1850),
             new Transactions.TransactionLine().fromSome('5', _getDate(2017, 5, 12, 13, 10, 0), Transactions.TransactionTypes.Spend, "rub", 400),
         ];
     }
